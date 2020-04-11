f8b17c0504e2043c3d98a3b25384f4c467ddd385
Tokzhan-Baz <b.tokzhan24@gmail.com>
2020-04-11 16:47:50 +0600

    UPDATE FOR URL POSTERS

--- WebApplication3/Controllers/MoviesController.cs
+++ WebApplication3/Controllers/MoviesController.cs
@@ -12,19 +12,19 @@ namespace WebApplication3.Controllers
         static List<Movie> movies = new List<Movie>
         {
             new Movie { Author = "Todd Phillips", Genre = "Crime , Drama , Thriller", Name = "Joker",
-                Poster = "https://dz7u9q3vpd4eo.cloudfront.net/admin-uploads/posters/mxt_movies_poster/joker_dabf394a-d4f2-4b68-90e2-011ed6b54012_poster.png?d=270x360&q=20",
+                Poster = "http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg",
                 CreatedAt = new DateTime(2019,10,3)
             },
             new Movie { Author = "David Leitch", Genre = "Action , Adventure", Name = "Fast & Furious Presents: Hobbs & Shaw",
-                Poster = "https://dz7u9q3vpd4eo.cloudfront.net/admin-uploads/posters/mxt_movies_poster/fast-furious-presents-hobbs-shaw_14d1ab4f-c90c-46d1-9e04-f7d69f285ebd_poster.png?d=270x360&q=20",
+                Poster = "https://camo.githubusercontent.com/2921202280866f3d1d35e68968b10ca1423d2254/68747470733a2f2f696d616765732d6e612e73736c2d696d616765732d616d617a6f6e2e636f6d2f696d616765732f4d2f4d5635424e6a6b324f44517a4e4459784e5635424d6c3542616e426e586b46745a5467774d5463794e4467344e6a45402e5f56315f55583138325f4352302c302c3138322c3236385f414c5f2e6a7067",
                 CreatedAt = new DateTime(2019,8,2)
             },
             new Movie { Author = "Jon Favreau", Genre = "Adventure , Animation , Drama , Family , Musical", Name = "The Lion King",
-                Poster = "https://dz7u9q3vpd4eo.cloudfront.net/admin-uploads/posters/mxt_movies_poster/the-lion-king_3904aadc-3a07-4836-892f-763b2dfdeea3_poster.png?d=270x360&q=20",
+                Poster = "https://camo.githubusercontent.com/03a3a78891c3d594d29f8d008d9bbf589b35c198/68747470733a2f2f696d616765732d6e612e73736c2d696d616765732d616d617a6f6e2e636f6d2f696d616765732f4d2f4d5635424d5463324d5451334d4441314e6c35424d6c3542616e426e586b46745a5467774f4441334f5449344e6a45402e5f56315f55583138325f4352302c302c3138322c3236385f414c5f2e6a7067",
                 CreatedAt = new DateTime(2019,7,19)
             },
             new Movie { Author = "Joachim Rønning", Genre = "Adventure , Family , Fantasy", Name = "Maleficent: Mistress of Evil",
-                Poster = "https://dz7u9q3vpd4eo.cloudfront.net/admin-uploads/posters/mxt_movies_poster/maleficent-mistress-of-evil_c8507e61-a6b3-404d-b8c5-df6f74bc62be_poster.png?d=270x360&q=20",
+                Poster = "https://camo.githubusercontent.com/21ed484b2dc8b4c1156069f29143d01f4bbb72c4/68747470733a2f2f696d616765732d6e612e73736c2d696d616765732d616d617a6f6e2e636f6d2f696d616765732f4d2f4d5635424e7a517a4f546b334f5441744e4451305a6930305a54566b4c5749304d5445744d446c6c5a6a4e6b597a4e6a4e5463344c326c745957646c586b4579586b467163476465515856794e6a55304f5451304f5459402e5f56315f55583138325f4352302c302c3138322c3236385f414c5f2e6a7067",
                 CreatedAt = new DateTime(2019,10,18)
             },
         };
